8ae7c632bfce7daba062e24363d4353f28009cbe
Kengo <83492993+KengoKato02@users.noreply.github.com>
2024-09-20 09:39:33 +0200

    all fixes

--- apps/api/src/index.js
+++ apps/api/src/index.js
@@ -1,16 +1,25 @@
-const express = require('express');
 const cors = require('cors');
+const express = require('express');
 
 const app = express();
-const port = process.env.PORT || 3000;
+const port = 3000;
 
-app.use(cors());
+app.use(
+  cors({
+    origin: 'http://localhost:3000',
+    methods: ['GET', 'POST', 'PUT', 'DELETE'],
+    allowedHeaders: ['Content-Type', '*']
+  })
+);
+
 app.use(express.json());
+app.disable('x-powered-by');
 
 app.get('/api', (req, res) => {
   res.json({ message: 'Welcome to the Travel Destinations Express API!' });
 });
 
 app.listen(port, () => {
-  console.log(`API server running at http://localhost:${port}`);
+  // Use a logger instead of console.log
+  // console.log(`API server running at http://localhost:${port}`);
 });
