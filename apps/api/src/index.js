8c6d363cacf585aed9196d4514bfd03b6f2100ba
Kengo <83492993+KengoKato02@users.noreply.github.com>
2024-09-24 12:48:27 +0200

    custom prod API

--- apps/api/src/index.js
+++ apps/api/src/index.js
@@ -9,11 +9,7 @@ const port = 3000;
 
 app.use(
   cors({
-    origin: [
-      'http://localhost:8080',
-      'http://127.0.0.1:8080',
-      'https://travel-destinations-mu.vercel.app/api'
-    ],
+    origin: ['*'],
     methods: ['GET', 'POST', 'PUT', 'DELETE'],
     allowedHeaders: ['Content-Type', '*']
   })
