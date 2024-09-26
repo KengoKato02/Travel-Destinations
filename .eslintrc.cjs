fad1a7750b27ea25e095b9790543a20ce6de6e66
Kengo <83492993+KengoKato02@users.noreply.github.com>
2024-09-26 10:39:26 +0200

    direct config for deploy

--- .eslintrc.cjs
+++ .eslintrc.cjs
@@ -11,7 +11,7 @@ module.exports = {
   },
   rules: {
     'n/no-process-env': 'off',
-    "import/no-relative-parent-imports": "off",
+    'import-x/extensions': 'off',
   },
   ignorePatterns: ['node_modules', 'dist'],
 };
