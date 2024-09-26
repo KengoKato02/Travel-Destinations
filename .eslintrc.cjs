6c25a4102b5150ff360c6418cda5940c4630e3d9
Kengo <83492993+KengoKato02@users.noreply.github.com>
2024-09-26 10:26:43 +0200

    pathing

--- .eslintrc.cjs
+++ .eslintrc.cjs
@@ -11,6 +11,8 @@ module.exports = {
   },
   rules: {
     'n/no-process-env': 'off',
+    'import/no-unresolved': 'error', // Reports unresolved imports
+    'import/named': 'error', // Reports missing named imports
   },
   ignorePatterns: ['node_modules', 'dist'],
 };
