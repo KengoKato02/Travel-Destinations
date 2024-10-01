d9fbbf1adb47dc623b1121838c0368eec8389e45
Kengo Kato <83492993+KengoKato02@users.noreply.github.com>
2024-10-01 15:28:41 +0200

    Revert "Revert "Change code to use mongoose""

--- apps/api/src/db/db.js
+++ apps/api/src/db/db.js
@@ -1,27 +1,10 @@
-import { MongoClient } from 'mongodb';
-
-let client;
-let clientPromise;
+import { mongoose } from 'mongoose';
 
 export async function connectToDatabase() {
-  if (!client) {
-    const uri = process.env.MONGODB_URI;
-
-    client = new MongoClient(uri, {
-      useUnifiedTopology: true,
-      minPoolSize: 5,
-      maxPoolSize: 10,
-      serverSelectionTimeoutMS: 50000
-    });
-
-    clientPromise = client.connect();
-  }
+  const uri = process.env.MONGODB_URI;
 
   try {
-    await clientPromise;
-    const db = client.db(process.env.MONGODB_DB_NAME);
-    console.log('Reusing MongoDB connection');
-    return db;
+    return mongoose.connect(uri);
   } catch (error) {
     console.error('Database connection error:', error);
     throw error;
