e97967426ecd216c2bce9927d44b3d239b9dda99
Patrik Hoferica <p.hoferica@outlook.com>
2024-10-09 18:26:28 +0200

    fe: login/signup logic

--- apps/api/src/index.js
+++ apps/api/src/index.js
@@ -1,6 +1,6 @@
-import cors from 'cors';
+import cors from "cors";
 
-import express from 'express';
+import express from "express";
 
 import {
   getHomeRoute,
@@ -8,15 +8,15 @@ import {
   getDestinationById,
   createDestination,
   updateDestination,
-  deleteDestination
-} from './controllers/destinations.js';
+  deleteDestination,
+} from "./controllers/destinations.js";
 
 import {
   getAllUsers,
   getUserByEmail,
   updateUser,
-  deleteUser
-} from './controllers/users.js';
+  deleteUser,
+} from "./controllers/users.js";
 
 import {
   getAllTrips,
@@ -24,16 +24,16 @@ import {
   createTrip,
   updateTrip,
   deleteTrip,
-  getTripsByUser
-} from './controllers/trips.js';
+  getTripsByUser,
+} from "./controllers/trips.js";
 
-import { config } from './db/config.js';
+import { config } from "./db/config.js";
 
-import { connectToDatabase } from './db/db.js';
+import { connectToDatabase } from "./db/db.js";
 
-import { validateObjectId } from './middleware/validateObjectId.js';
+import { validateObjectId } from "./middleware/validateObjectId.js";
 
-import { verifyAdmin, verifyToken } from './controllers/auth.js';
+import { verifyAdmin, verifyToken, login, signup } from "./controllers/auth.js";
 
 const app = express();
 
@@ -51,18 +51,18 @@ function setupMiddleware() {
   app.use(
     cors({
       origin: [
-        'https://travel-destinations-mu.vercel.app',
-        'http://localhost:8080',
-        'http://127.0.0.1:8080'
+        "https://travel-destinations-mu.vercel.app",
+        "http://localhost:8080",
+        "http://127.0.0.1:8080",
       ],
-      methods: ['GET', 'POST', 'PUT', 'DELETE', 'OPTIONS'],
-      allowedHeaders: ['Content-Type']
+      methods: ["GET", "POST", "PUT", "DELETE", "OPTIONS"],
+      allowedHeaders: ["Content-Type"],
     })
   );
 
   app.use(express.json());
 
-  app.disable('x-powered-by');
+  app.disable("x-powered-by");
 }
 
 async function startServer() {
@@ -72,69 +72,72 @@ async function startServer() {
     app.listen(PORT, () => {
       console.log(
         `Server running in ${
-          config.isProduction ? 'production' : 'development'
+          config.isProduction ? "production" : "development"
         } mode on port ${PORT}`
       );
     });
   } catch (error) {
-    console.error('Failed to start the server:', error);
+    console.error("Failed to start the server:", error);
 
     process.exit(1);
   }
 }
 
 function setupRoutes() {
-  app.options('*', cors());
+  app.options("*", cors());
 
   // DESTINATION ROUTES
-  app.get('/api/v1', (req, res) => getHomeRoute(req, res));
+  app.get("/api/v1", (req, res) => getHomeRoute(req, res));
 
-  app.get('/api/v1/destinations', verifyToken, (req, res) =>
+  app.get("/api/v1/destinations", verifyToken, (req, res) =>
     getAllDestinations(req, res)
   );
 
-  app.get('/api/v1/destinations/:id', verifyToken, (req, res) =>
+  app.get("/api/v1/destinations/:id", verifyToken, (req, res) =>
     getDestinationById(req, res)
   );
 
-  app.post('/api/v1/destinations', verifyToken, verifyAdmin, (req, res) =>
+  app.post("/api/v1/destinations", verifyToken, verifyAdmin, (req, res) =>
     createDestination(req, res)
   );
 
-  app.put('/api/v1/destinations/:id', verifyToken, verifyAdmin, (req, res) =>
+  app.put("/api/v1/destinations/:id", verifyToken, verifyAdmin, (req, res) =>
     updateDestination(req, res)
   );
 
-  app.delete('/api/v1/destinations/:id', verifyToken, verifyAdmin, (req, res) =>
+  app.delete("/api/v1/destinations/:id", verifyToken, verifyAdmin, (req, res) =>
     deleteDestination(req, res)
   );
 
   // USER ROUTES
-  app.get('/api/v1/users', verifyToken, (req, res) => getAllUsers(req, res));
+  app.get("/api/v1/users", verifyToken, (req, res) => getAllUsers(req, res));
 
-  app.get('/api/v1/users/:email', verifyToken, (req, res) =>
+  app.get("/api/v1/users/:email", verifyToken, (req, res) =>
     getUserByEmail(req, res)
   );
 
   // AUTHENTICATION ROUTES
-  app.put('/api/v1/users/:email', verifyToken, verifyAdmin, (req, res) =>
+  app.put("/api/v1/users/:email", verifyToken, verifyAdmin, (req, res) =>
     updateUser(req, res)
   );
 
-  app.delete('/api/v1/users/:email', verifyToken, verifyAdmin, (req, res) =>
+  app.delete("/api/v1/users/:email", verifyToken, verifyAdmin, (req, res) =>
     deleteUser(req, res)
   );
 
+  app.post("/api/v1/auth/login", login);
+  app.post("/api/v1/auth/signup", signup);
+
   // TRIP ROUTES
-  app.get('/api/v1/trips', (req, res) => getAllTrips(req, res));
+  app.get("/api/v1/trips", (req, res) => getAllTrips(req, res));
 
-  app.get('/api/v1/trips/:id', validateObjectId('id'), getTripById);
+  app.get("/api/v1/trips/:id", validateObjectId("id"), getTripById);
 
-  app.post('/api/v1/trips', (req, res) => createTrip(req, res));
+  app.post("/api/v1/trips", (req, res) => createTrip(req, res));
 
-  app.put('/api/v1/trips/:id', validateObjectId('id'), updateTrip);
+  app.put("/api/v1/trips/:id", validateObjectId("id"), updateTrip);
 
-  app.delete('/api/v1/trips/:id', validateObjectId('id'), deleteTrip);
+  app.delete("/api/v1/trips/:id", validateObjectId("id"), deleteTrip);
 
-  app.get('/api/v1/trips/user/:id', validateObjectId('id'), getTripsByUser);
+  app.get("/api/v1/trips/user/:id", validateObjectId("id"), getTripsByUser);
 }
